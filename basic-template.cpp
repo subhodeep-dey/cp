81b01b293195c0a506d6ddca81b2f9b7482d0a15
subhodeep-dey <90602098+subhodeep-dey@users.noreply.github.com>
2021-09-13 13:01:45 +0530

    Create basic-template.cpp

---
+++ basic-template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void solve() {
+    int n, s, div, qnt;
+    cin >> n >> s;
+    div = n/2 + 1;
+    qnt = s/div;
+    cout << qnt << endl;
+ 
+return;
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0); cout.tie(0);
+    int t = 1;  
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
