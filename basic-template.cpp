fb649afcf51d785fbfd74095da66d477d3ca31c8
subhodeep-dey <90602098+subhodeep-dey@users.noreply.github.com>
2021-09-13 14:10:45 +0530

    Update basic-template.cpp

--- basic-template.cpp
+++ basic-template.cpp
@@ -1,21 +1,28 @@
 #include <bits/stdc++.h>
+#define Code ios_base::sync_with_stdio(false);
+#define by cin.tie(NULL);
+#define black_heretic cout.tie(NULL);
  
 using namespace std;
+
+void online_judge(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+//Code by Subhodeep Dey. © 2021.
  
 void solve() {
-    int n, s, div, qnt;
-    cin >> n >> s;
-    div = n/2 + 1;
-    qnt = s/div;
-    cout << qnt << endl;
- 
+ //Code here
 return;
 }
  
 signed main(){
-    ios_base::sync_with_stdio(false); 
-    cin.tie(0); cout.tie(0);
-    int t = 1;  
+    Code by black_heretic
+    online_judge();
+    int t;  
     cin >> t;
     while(t--) solve();
     return 0;
